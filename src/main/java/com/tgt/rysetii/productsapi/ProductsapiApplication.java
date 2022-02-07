e1f55803380b6e74e3505036f1193ae305483079
Anitha.R <anitha.r@target.com>
2022-02-07 16:59:33 +0530

    Added Hello world

---
+++ src/main/java/com/tgt/rysetii/productsapi/ProductsapiApplication.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.productsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/productsapi/ProductsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
